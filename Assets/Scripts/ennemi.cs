e3d8d6b8d96e52a44f9edd4cde4e89ae215045ff
Yashirizu <118368064+MathisVchia@users.noreply.github.com>
2023-10-25 18:08:37 +0200

    Déplacement_Op

--- Assets/Scripts/ennemi.cs
+++ Assets/Scripts/ennemi.cs
@@ -6,32 +6,49 @@ public class ennemi : MonoBehaviour
 {
 
     public GameObject mobs;
-    public Player speed;
+    public Player player;
     private Transform limitL;
     private Transform limitR;
+    private bool direction;
 
     public ReferenceLibrary referenceLibrary;
     // Start is called before the first frame update
     void Start()
     {
+
         referenceLibrary = FindObjectOfType<ReferenceLibrary>();
         limitL = referenceLibrary.LimitL;
         limitR = referenceLibrary.LimitR;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        mobs.transform.position += Vector3.left * 0.002f;
 
-        if (transform.position.x < limitL.position.x)
+        if (transform.position.x <= limitL.position.x)
         {
             mobs.transform.position += Vector3.right * 0.002f;
+            direction = false;
         }
 
-        if (transform.position.x > limitR.position.x)
+        if (transform.position.x >= limitR.position.x)
         {
             mobs.transform.position += Vector3.left * 0.002f;
+            direction = true;
+        }
+
+        else
+        {
+            if (direction)
+            {
+                mobs.transform.position += Vector3.left * 0.002f;
+            }
+            else
+            {
+                mobs.transform.position += Vector3.right * 0.002f;
+            }
+            
         }
     }
 }
